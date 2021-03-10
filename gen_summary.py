7f9649cc5744ae12a97cc5beed803ce86021be0e
Aleksey Parfyonov <kantengri3@gmail.com>
2021-03-10 13:57:23 +0500

    sanitazed; not work

--- gen_summary.py
+++ gen_summary.py
@@ -1,25 +1,153 @@
-import sys
-from utils import read_transactions
+import sys, os, re, datetime, json
+from utils import read_transactions, read_alfa
 import argparse, logging
+from collections import namedtuple, defaultdict
+from typing import List, DefaultDict, Dict, Union
+from mypy_extensions import TypedDict
 
+uni_t = namedtuple('uni_t', ['account', 'date', 'amount', 'currency', 'category', 'src'])
 
-def make_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('csvdir')
-    parser.add_argument('--out', default="summary.json")
-    return parser
+class list_and_float_t:
+    def __init__(self, v=0., trs=[]):
+        self.v : float = v
+        self.trs : List[uni_t] = trs
 
-def main(self, args):
-    logging.basicConfig(level=logging.INFO)
-    Main(args).main()
+    def __add__(self, o):
+        return list_and_float_t(self.v + o.v, self.trs + o.trs)
 
 class Main:
+    @staticmethod
+    def make_parser():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('csvdir')
+        parser.add_argument('--debug', action='store_true')
+        parser.add_argument('--days_ago', type=int)
+        parser.add_argument('--allout', default="transactions.json")
+        parser.add_argument('--sumout', default="summary.json")
+        return parser
+
     def  __init__(self, args):
         self.args = args
-        self.logger = logging.getLogger(self._class.__name__)
+        self.logger = logging.getLogger(self.__class__.__name__)
 
-    def main(self):
-        pass
+    def read_datadir(self):
+        for f in os.listdir(self.args.csvdir):
+            if f.endswith('.csv'):
+                try:
+                    fullpath = os.path.join(self.args.csvdir,f)
+                    if 'rub' in f:
+                        trs = read_alfa(fullpath)
+                        yield 'rub', trs
+                    elif 'credit' in f:
+                        trs = read_alfa(fullpath)
+                        yield 'credit', trs
+                    elif 'byn' in f:
+                        trs = read_transactions(fullpath)
+                        yield 'byn', trs
+                    elif 'usd' in f:
+                        trs = read_transactions(fullpath)
+                        yield 'usd', trs
+                    else:
+                        self.logger.warning(f"{fullpath} is not recognized")
+                except Exception:
+                    self.logger.exception(fullpath)
+                    raise
+
+    def to_unified_rec(self, en):
+        if self.args.days_ago is not None:
+            min_date = datetime.today() - datetime.timedelta(days=self.args.days_ago)
+            self.logger.info(f"min_date {min_date}")
+
+        for _type, trs in en:
+            #['date', 'amount', 'currency', 'category', 'src']
+            for tr in trs:
+                if self.args.days_ago is not None:
+                    if tr.date < min_date:
+                        continue
+                if _type in ['rub', 'credit']:
+                    yield uni_t(_type, tr.date, tr.amount, tr.currency, tr.category, tr)
+                else:
+                    yield uni_t(_type, tr.date, tr.amount, tr.currency, tr.op, tr)
+
+    def get_category(self, tr : uni_t):
+        cat_defs = {
+            'sadik' : [r'Парфенов\s+Сергей']
+        }
+        for cat, regs in cat_defs.items():
+            for reg in regs:
+                ret = re.search(reg, tr.category)
+                if ret:
+                    return cat
+            if tr.amount > 0:
+                return 'income'
+            return 'other'
+
+    def group_by_category(self, en : List[uni_t]):
+        # ValueOrlistDict = TypedDict('ValueOrlistDict', {
+        #         'v'   : float,
+        #         'trs' : List[uni_t]
+        #     })
+
+        summary : DefaultDict[str,DefaultDict[str, DefaultDict[str, list_and_float_t]]] = \
+            defaultdict(lambda: defaultdict(lambda: defaultdict(list_and_float_t)))
+
+        for tr in en:
+            cat = self.get_category(tr)
+            summary[cat][tr.account][tr.currency].v += tr.amount
+            summary[cat][tr.account][tr.currency].trs += [tr]
+
+        cat_totals : DefaultDict[str,DefaultDict[str,list_and_float_t]] = defaultdict(lambda: defaultdict(list_and_float_t))
+        acc_totals : DefaultDict[str,DefaultDict[str,list_and_float_t]] = defaultdict(lambda: defaultdict(list_and_float_t))
+        spent_total : DefaultDict[str,list_and_float_t] = defaultdict(list_and_float_t)
+
+        for cat, accd in summary.items():
+            for acc, curd in accd.items():
+                for cur, v in curd.items():
+                    cat_totals[cat][cur] += v
+                    acc_totals[acc][cur] += v
+                    spent_total[cur] += v if cat != 'income' else list_and_float_t()
+
+        return {
+            'summary' : summary, 
+            'cat_totals' : cat_totals, 
+            'acc_totals' : acc_totals, 
+            'spent_total' : spent_total
+            }
+
+    def sanitize(self, en):
+        if isinstance(en, list_and_float_t):
+            return list_and_float_t(en.v, self.sanitize(en.trs))
+        if isinstance(en, list):
+            return [self.sanitize(e) for e in en]
+        elif isinstance(en, dict):
+            return { k : self.sanitize(v) for k,v in en.items()}
+        elif hasattr(en, '_fields'):
+            return { k : self.sanitize(v) for k,v in zip(en._fields, en)}
+        return en
+
+    def go(self):
+        en = self.read_datadir()
+        en = self.to_unified_rec(en)
+        en = list(en)
+        en = sorted(en, key=lambda tr: tr.date)
+
+        def sterilize(obj):
+            if isinstance(obj, datetime.datetime):
+                return str(obj)
+            return vars(obj)
+
+        with open(self.args.allout, 'w') as f:
+            json.dump(self.sanitize(en), f, indent=4, default=sterilize, ensure_ascii=False)
+
+        en = self.group_by_category(en)
+
+        with open(self.args.sumout, 'w') as f:
+            json.dump(self.sanitize(en), f, indent=4, default=sterilize, ensure_ascii=False)
+
+    @staticmethod
+    def main(args):
+        logging.basicConfig(level=logging.INFO if not args.debug else logging.DEBUG)
+        Main(args).go()
 
 if __name__ == "__main__":
-    main(make_parser().parse_args(sys.argv))
+    Main.main(Main.make_parser().parse_args())
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
-import csv,re, datetime
+import csv,re, datetime, os
 from collections import namedtuple
 
-rec_t = namedtuple('rec_t', ['date', 'op', 'amount', 'currency', 'amount_byn', 'category'])
+rec_t = namedtuple('rec_t', ['date', 'op', 'amount', 'currency', 'amount_byn', 'category', 'filename', 'lineno'])
 
 def read_transactions(filename):
     transactions = []
@@ -9,7 +9,7 @@ def read_transactions(filename):
 
     with open(filename, 'r', encoding="windows-1251") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
-        for row in csv_reader:
+        for lineno, row in enumerate(csv_reader):
             # ret = re.match(r'Операции по.*?(\d+)', row[0]):
             if len(row) < 10:
                 # print(row)
@@ -25,6 +25,34 @@ def read_transactions(filename):
                 category = row[-2]
                 if len(category.strip()) == 0:
                     category = op
-                transactions.append(rec_t(tstamp, op, amount, currency, amount_byn, category))
+                transactions.append(rec_t(tstamp, op, amount, currency, amount_byn, category, os.path.basename(filename), lineno))
+
+    return transactions
+
+
+alfa_t = namedtuple('alfa_t', ['account_name', 'account_num', 'currency', 'date', 'ref', 'category', 'amount', 'filename', 'lineno'])
+
+def read_alfa(filename):
+    transactions = []
+    # Тип счёта Номер счета Валюта  Дата операции   Референс проводки   Описание операции   Приход  Расход
+
+    with open(filename, 'r', encoding="windows-1251") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=';')
+        for lineno, row in enumerate(csv_reader):
+            try:
+                account_name = row['Тип счёта']
+                if len(account_name) == 0:
+                    continue
+                account_num  = row['Номер счета']
+                currency     = row['Валюта']
+                tstamp       = datetime.datetime.strptime(row['Дата операции'], '%d.%m.%y')
+                ref          = row['Референс проводки']
+                category     = row['Описание операции']
+                income       = float(row['Приход'].replace(',','.'))
+                spent        = float(row['Расход'].replace(',','.'))
+                amount       = income - spent
+                transactions.append(alfa_t(account_name, account_num, currency, tstamp, ref, category, amount, os.path.basename(filename), lineno))
+            except Exception as e:
+                raise Exception(f"line {lineno}", e)
 
     return transactions
