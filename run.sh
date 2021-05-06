adf19b619c8cd5ed5cec66469d0961dc85daf918
Aleksey Parfyonov <kantengri3@gmail.com>
2021-05-06 10:53:02 +0500

    venv system pkg

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 
 [ -d "env3" ] || {
-    python3 -m venv env3
+    python3 -m venv --system-site-packages env3
 }
 . env3/bin/activate
-python3 -m pip install -r requirements.txt
-python3 app.py
+python -m pip install -r requirements.txt
+python app.py
